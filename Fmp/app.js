f071b6d6693053f0f1e36da5d143abbd5f284ad7
FatimaZohra313 <108746799+FatimaZohra313@users.noreply.github.com>
2022-08-23 04:51:20 -0700

    Add files via upload

---
+++ Fmp/app.js
@@ -0,0 +1,204 @@
+var h3 = document.getElementById("h3");
+h3.innerHTML = "Contact us : 0321-2807969";
+var id1 = document.getElementById("id1");
+id1.innerHTML = "Wrap Yourself in KASMAFHZ"
+var p1 = document.getElementById("p1");
+p1.innerHTML = "KASMAFHZ is the people’s first choice to get their hands on authentic, latest collection of ladies suits. Spare yourself the struggle to visit each of your favorite brands’ pages separately. Have access to the most royal and regal unstitched collection from all your favorite brands on our websites"
+var p2 = document.getElementById("p2");
+p2.innerHTML="We are serving you quality – rich, intricately designed unstitched collection to suit your style. At our online store, you have a unique, colorful, and extensive collection to choose from. We have the most magnificent formal collection, lawn collection, bridal dresses, and much more."
+function change(e){
+    e.src = images/img4.jpeg;
+
+}
+function cbyNew(){
+
+ document.getElementById("image8").src="images/img11.jpeg";
+  
+}
+function cbyOld(){
+  
+    document.getElementById("image8").src="images/image11.jpeg";
+    
+
+}
+
+function ruedadNew(){
+    document.getElementById("image1").src="images/img4.jpeg";
+    
+
+}
+function ruedadOld(){
+    document.getElementById("image1").src="images/img2.jpeg";
+
+
+}
+function zcmNew(){
+    document.getElementById("image2").src="images/img5(2).jpeg";
+    
+}
+function zcmOld(){
+    document.getElementById("image2").src="images/img5.jpeg";
+
+}
+function eacNew(){
+    document.getElementById("image3").src="images/img7.jpeg";
+
+}
+function eacOld(){
+    document.getElementById("image3").src="images/image7(2).jpeg";
+
+}
+function zcNew(){
+    document.getElementById("image4").src="images/image6.jpeg";
+
+}
+function zcOld(){
+    document.getElementById("image4").src="images/images6.jpeg";
+
+
+}
+function cylNew(){
+    document.getElementById("image5").src="images/img8.jpeg";
+    
+}
+function cylOld(){
+    document.getElementById("image5").src="images/image8.jpeg"
+
+}
+function mllnew(){
+    document.getElementById("image6").src="images/image9.jpeg";
+    
+}
+function mllOld(){
+    document.getElementById("image6").src="images/img9.jpeg";
+
+}
+function myNew(){
+    document.getElementById("image7").src="images/img10.jpg";
+    
+}
+function myOld(){
+    document.getElementById("image7").src="images/image10.jpg";
+
+}
+function mlNew(){
+    document.getElementById("image9").src="../images/image9.jpeg";
+    
+}
+function mlOld(){
+    document.getElementById("image9").src="../images/img9.jpeg";
+
+}
+function zclNew(){
+    document.getElementById("image10").src="../images/img8.jpeg";
+}
+function zclOld(){
+    document.getElementById("image10").src="../images/image8.jpeg";
+}
+function cyblNew(){
+    document.getElementById("image11").src="../images/img11.jpeg";
+    
+}
+function cyblOld(){
+    document.getElementById("image11").src="../images/image11.jpeg";
+
+}
+function newImage(){
+   
+    document.getElementById("image12").src="../images/image12.jpeg";
+
+}
+function oldImage(){
+
+    document.getElementById("image12").src="../images/img12.jpeg";
+}
+function mushqredNew(){
+    document.getElementById("image13").src="../images/image13.jpeg";
+}
+function mushqredOld(){
+    document.getElementById("image13").src="../images/img13.jpeg";
+}
+function bllNew(){
+    document.getElementById("image14").src="../images/img14.jpeg";
+}
+function bllOld(){
+    document.getElementById("image14").src="../images/image14.jpeg";
+}
+function gaNew(){
+    document.getElementById("image15").src="../images/image16.jpeg";
+}
+function gaOld(){
+    document.getElementById("image15").src="../images/img16.jpeg";
+}
+function mahiNew(){
+    document.getElementById("image16").src="../images/image15.jpeg";
+}
+function mahiOld(){
+    document.getElementById("image16").src="../images/img15.jpeg";
+}
+function mushqLuxuryNew(){
+    document.getElementById("image17").src="../images/image17.jpeg";
+}
+function mushqLuxuryOld(){
+    document.getElementById("image17").src="../images/img17.jpeg";
+}
+function mbNew(){
+    document.getElementById("image18").src="../images/image18.jpeg";
+}
+function mbOld(){
+    document.getElementById("image18").src="../images/img18.jpeg";
+}
+function anayaNew(){
+    document.getElementById("image19").src="../images/image20.jpeg";
+}
+function anayaOld(){
+    document.getElementById("image19").src="../images/img20.jpeg";
+}
+function mushqBlackNew(){
+    document.getElementById("image20").src="../images/image19.jpeg";
+}
+function mushqBlackOld(){
+    document.getElementById("image20").src="../images/img19.jpeg";
+}
+function zainabMagnetaNew(){
+    document.getElementById("image21").src="../images/img5(2).jpeg";
+}
+function zainabMagnetaOld(){
+    document.getElementById("image21").src="../images/img5.jpeg";
+}
+function saharNew(){
+    document.getElementById("image22").src="../images/img2.jpeg";
+}
+function saharOld(){
+    document.getElementById("image22").src="../images/img4.jpeg";
+}
+function aghaNew(){
+    document.getElementById("image23").src="../images/image21.jpeg";
+}
+function aghaOld(){
+    document.getElementById("image23").src="../images/img21.jpeg";
+}
+function mwlNew(){
+    document.getElementById("image24").src="../images/image23.jpeg";
+}
+function mwlOld(){
+    document.getElementById("image24").src="../images/img23.jpeg";
+}
+function mocNew(){
+    document.getElementById("image25").src="../images/image22.jpeg";
+}
+function mocOld(){
+    document.getElementById("image25").src="../images/img22.jpeg";
+}
+function qlcNew(){
+    document.getElementById("image26").src="../images/img24.jpeg";
+}
+function qlcOld(){
+    document.getElementById("image26").src="../images/image24.jpeg";
+}
+function zcsgNew(){
+    document.getElementById("image27").src="../images/img25.jpeg";
+}
+function zcsgOld(){
+    document.getElementById("image27").src="../images/image25.jpeg";
+}
